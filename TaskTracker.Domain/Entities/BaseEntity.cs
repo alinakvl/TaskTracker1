9bb55f8d0d76a11e5a93da5a383b662832255523
Alina <alina.koval@lnu.edu.ua>
2025-12-05 14:23:41 +0200

    Entities

--- TaskTracker.Domain/Entities/BaseEntity.cs
+++ TaskTracker.Domain/Entities/BaseEntity.cs
@@ -1,10 +1,8 @@
 ﻿namespace TaskTracker.Domain.Entities;
-public abstract class BaseEntity<TId>
-{ 
-    public TId Id { get; set; } = default!;
+public abstract class BaseEntity
+{  
+    public Guid Id { get; set; }
     public DateTime CreatedAt { get; set; }
-    public DateTime? UpdatedAt { get; set; }
-    public string CreatedBy { get; set; } = string.Empty;
-    public string? UpdatedBy { get; set; }
+    public DateTime UpdatedAt { get; set; }
     public bool IsDeleted { get; set; }
 }
