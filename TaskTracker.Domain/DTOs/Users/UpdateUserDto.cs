af526c2b861b1e0ccb0891d5af5a365b12d516f3
Alina <alina.koval@lnu.edu.ua>
2025-12-05 14:51:39 +0200

    UserDTO

---
+++ TaskTracker.Domain/DTOs/Users/UpdateUserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskTracker.Domain.DTOs.Users;
+public class UpdateUserDto
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string? AvatarUrl { get; set; }
+}
+
---
+++ TaskTracker.Domain/DTOs/Users/UserDto.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskTracker.Domain.DTOs.Users;
+
+public class UserDto
+{
+    public Guid Id { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string FullName => $"{FirstName} {LastName}";
+    public string? AvatarUrl { get; set; }
+    public string Role { get; set; } = string.Empty;
+}
+
