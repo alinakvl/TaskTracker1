9b2d9527bd45f36b289905cf37b30239449ec3ce
Alina <alina.koval@lnu.edu.ua>
2025-12-12 21:45:43 +0200

    Activities. The LogActivityCommand is not yet integrated into other handlers, so the returned lists are currently empty

---
+++ TaskTracker.Application/Commands/Activities/LogActivity/LogActivityCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using TaskTracker.Domain.Entities;
+using TaskTracker.Application.Interfaces.Repositories;
+
+namespace TaskTracker.Application.Commands.Activities.LogActivity;
+internal class LogActivityCommandHandler : IRequestHandler<LogActivityCommand, Guid>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public LogActivityCommandHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Guid> Handle(LogActivityCommand request, CancellationToken cancellationToken)
+    {
+        var activity = new Activity
+        {
+            Id = Guid.NewGuid(),
+            UserId = request.UserId,
+            ActionType = request.ActionType,
+            EntityType = request.EntityType,
+            EntityId = request.EntityId,
+            BoardId = request.BoardId,
+            OldValues = request.OldValues,
+            NewValues = request.NewValues,
+            IPAddress = request.IPAddress,
+            UserAgent = request.UserAgent,
+            CreatedAt = DateTime.UtcNow
+        };
+
+        await _unitOfWork.Activities.AddAsync(activity, cancellationToken);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return activity.Id;
+    }
+}
---
+++ TaskTracker.Application/Queries/Activities/GetActivitiesByBoard/GetActivitiesByBoardQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using TaskTracker.Domain.Entities;
+using TaskTracker.Application.Interfaces.Repositories;
+
+namespace TaskTracker.Application.Queries.Activities.GetActivitiesByBoard;
+internal class GetActivitiesByBoardQueryHandler : IRequestHandler<GetActivitiesByBoardQuery, IEnumerable<Activity>>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public GetActivitiesByBoardQueryHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<IEnumerable<Activity>> Handle(GetActivitiesByBoardQuery request, CancellationToken cancellationToken)
+    {
+        var activities = await _unitOfWork.Activities.FindAsync(
+            a => a.BoardId == request.BoardId,
+            cancellationToken);
+
+        return activities
+            .OrderByDescending(a => a.CreatedAt)
+            .Take(request.Take)
+            .ToList();
+    }
+}
